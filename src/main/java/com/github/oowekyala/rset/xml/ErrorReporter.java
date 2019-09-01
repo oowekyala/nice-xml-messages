82dcdb12a804ff2630fd0efc02c06c7633b1bda1
Clément Fournier <clement.fournier76@gmail.com>
2019-09-01 13:32:48 +0200

    Initial commit

---
+++ src/main/java/com/github/oowekyala/rset/xml/ErrorReporter.java
@@ -0,0 +1,38 @@
+package com.github.oowekyala.rset.xml;
+
+import java.util.List;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public interface ErrorReporter {
+
+
+    void warn(Node node, String message);
+
+
+    void warn(NodeList node, String message);
+
+
+    XmlParsingException error(Node node, String message);
+
+
+    XmlParsingException error(Node node, Throwable ex);
+
+
+    XmlParsingException error(NodeList node, String message);
+
+
+    class XmlParsingException extends RuntimeException {
+
+        private final List<XmlParsingException> aggregate;
+
+        public XmlParsingException(List<XmlParsingException> aggregate) {
+            this.aggregate = aggregate;
+        }
+
+        // todo position
+
+
+    }
+}
---
+++ src/main/java/com/github/oowekyala/rset/xml/RuleDefBuilder.java
@@ -0,0 +1,16 @@
+package com.github.oowekyala.rset.xml;
+
+
+public interface RuleDefBuilder {
+
+
+    void name(String lang);
+
+
+    void language(String lang);
+
+
+    void clazz(String cname);
+
+
+}
