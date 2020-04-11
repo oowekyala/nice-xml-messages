dbaceeae12d6d6f4a8de6f393bd9f348f75333fd
Clément Fournier <clement.fournier76@gmail.com>
2020-04-12 00:50:33 +0200

    Add utils

--- src/main/java/com/github/oowekyala/ooxml/DomUtils.java
+++ src/main/java/com/github/oowekyala/ooxml/DomUtils.java
@@ -1,10 +1,16 @@
 package com.github.oowekyala.ooxml;
 
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
+import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -13,6 +19,32 @@ import org.w3c.dom.NodeList;
  * Simple utilities to work with the {@code org.w3c.dom} API.
  */
 public final class DomUtils {
+
+    /**
+     * Returns the list of children of the given element
+     * that are also elements.
+     */
+    public static List<Element> elementsIn(Element element) {
+        List<Node> nodes = new ArrayList<>(asList(element.getChildNodes()));
+        nodes.removeIf(n -> !(n instanceof Element));
+        return (List) nodes;
+    }
+
+
+    /**
+     * Returns the value of the attribute with the given name
+     * as an optional.
+     *
+     * @param element       Element to get the attribute from
+     * @param attributeName Attribute name (local name, no NS)
+     */
+    public static Optional<String> getAttributeOpt(Element element, String attributeName) {
+        if (element.hasAttribute(attributeName)) {
+            return Optional.ofNullable(element.getAttribute(attributeName));
+        }
+        return Optional.empty();
+    }
+
 
     /**
      * Returns an unmodifiable view of the given nodelist as a list.
